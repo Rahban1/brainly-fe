04f8b30cb7e9884ec4f83e82c629756cf3e73c98
rahban <rahban.ghani2001@gmail.com>
2025-02-03 18:48:34 +0530

    added brain gif on the home page -2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,19 @@ export function Navbar() {
     const navigate = useNavigate();
 
     return (
-        <div className="w-full flex justify-between items-center px-2 md:px-8 py-3 bg-black">
+        <div className=" w-full flex justify-between items-center px-2 md:px-8 py-3 bg-black">
             <div className="flex justify-center items-center gap-1">
                 <button onClick={() => { navigate('/') }}>
                     <img className="w-10 h-10" src="/brain.svg" alt="logo" />
                 </button>
-                <div onClick={()=> {navigate('/')}} className="cursor-pointer font-head hidden sm:block text-white">
+                <div onClick={()=> {navigate('/')}} className="cursor-pointer text-lg font-head hidden sm:block text-white">
                     Recollectify
                 </div>
             </div>
             <div className="flex items-center gap-2">
                 <button 
                     onClick={() => { navigate('/signin')}} 
-                    className="relative mt-2 ml-2 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-semibold rounded-lg group 
+                    className="relative mt-2 ml-2 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden md:text-lg font-semibold rounded-lg group 
                              bg-gradient-to-br from-purple-600 via-purple-500 to-pink-500 
                              hover:from-pink-500 hover:via-purple-500 hover:to-purple-600 
                              transition-all duration-300 ease-in-out 
@@ -32,7 +32,7 @@ export function Navbar() {
                 </button>
                 <button 
                     onClick={() => { navigate('/signup')}} 
-                    className="relative mt-2 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-lg font-semibold rounded-lg group 
+                    className="relative mt-2 inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden md:text-lg font-semibold rounded-lg group 
                              bg-gradient-to-br from-purple-600 via-purple-500 to-pink-500 
                              hover:from-pink-500 hover:via-purple-500 hover:to-purple-600 
                              transition-all duration-300 ease-in-out 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,14 +15,17 @@ export function Home() {
     };
 
     return (
-      <div className='w-full h-full bg-black'>
+      <div className='w-full h-screen bg-black'>
         <Navbar/>
         <div className='flex justify-center mt-14 items-center text-center text-white'>
           <div>
-            <div className='text-5xl font-head my-6 md:my-8 md:text-7xl'>Your Mind's Archive</div>
-            <div className='text-sm md:text-lg font-body w-[75%] mx-auto text-gray-300 mb-6 md:mb-8'>
+            <div className='text-5xl font-head my-6 md:mt-4 md:text-7xl'>Your Mind's Archive</div>
+            <div className='text-sm md:text-lg font-body w-[75%] mx-auto text-gray-300 mt-4 md:mt-6'>
                 Save & Revisit Your Favorite things on the Internet Effortlessly
             </div>
+            <div className='w-[60%] mx-auto'>
+                    <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3AxbjQ5YnZ5YnF6dTczMG5wMDA4eXR5Z28xbXpvOGNuOHgzcHRzaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YS57N6teaevJASvcMA/giphy.gif" alt="" />
+                </div>
             <div className='flex justify-center flex-col gap-2'>
                 <button 
                     onClick={handleStartCollecting} 
@@ -54,9 +57,7 @@ export function Home() {
                     </span>
                 </button>
 
-                <div className='w-[66%] mx-auto'>
-                    <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3AxbjQ5YnZ5YnF6dTczMG5wMDA4eXR5Z28xbXpvOGNuOHgzcHRzaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YS57N6teaevJASvcMA/giphy.gif" alt="" />
-                </div>
+                
             </div>
           </div>
         </div>
